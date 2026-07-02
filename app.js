312bd0f17ff2e6f845bbd1af8d66bc2002de44c5
tyh <2939056298@qq.com>
2026-07-02 09:51:45 +0800

    修复了拿到body是乱码的问题

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ const cors = require('cors');
 
 const app = express();
 
+// 这个记得放在路由前面解析，不然路由拿到的body是乱码
+app.use(express.urlencoded({ extended: false }));
 
 const userRouter = require('./router/user');
 app.use('/api', userRouter);
 
 app.use(cors());
 
-app.use(express.urlencoded({ extended: false }));
+
+// app.use(express.json());
 
 app.listen(3007, () => {
     console.log('Server is running on http://127.0.0.1:3007');
